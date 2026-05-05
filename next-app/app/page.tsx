89cde5cebf88631c26220b1aa15ed611fd7b6619
Fernando <fernandomateos@gmail.com>
2026-05-05 20:09:53 -0300

    Add Instagram links to bio (EN + ES)

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -185,27 +185,27 @@ export default function HomePage() {
             </p>
             <p>
               Thanks to this career I’ve been lucky to live and work in several countries in
-              Europe, America, Africa and Australia, doing amazing things like{‘ ‘}
+              Europe, America, Africa and Australia, doing amazing things like{' '}
               <a href="https://www.instagram.com/p/Bfi9ZffFWR9/" target="_blank" rel="noreferrer">
                 tracking lynx in the snow
               </a>
-              , guiding{‘ ‘}
+              , guiding{' '}
               <a href="https://www.instagram.com/p/BckoAqDjUU0/" target="_blank" rel="noreferrer">
                 expeditions in the Peruvian Amazon
               </a>
-              ,{‘ ‘}
+              ,{' '}
               <a href="https://www.instagram.com/p/BoHZkGpH7do/" target="_blank" rel="noreferrer">
                 canoeing down the Yukon River in Canada
               </a>
-              ,{‘ ‘}
+              ,{' '}
               <a href="https://www.instagram.com/p/BPYdq4fj_RR/" target="_blank" rel="noreferrer">
                 diving in Lake Tanganyika
               </a>
-              ,{‘ ‘}
+              ,{' '}
               <a href="https://www.instagram.com/p/BiCkZJZHYKV/" target="_blank" rel="noreferrer">
                 exploring the island of Trinidad
               </a>
-              {‘ ‘}and even{‘ ‘}
+              {' '}and even{' '}
               <a href="https://www.instagram.com/p/BqYTnoVhHWR/" target="_blank" rel="noreferrer">
                 working in the rainforest with the BBC for a Sir David Attenborough series
               </a>
